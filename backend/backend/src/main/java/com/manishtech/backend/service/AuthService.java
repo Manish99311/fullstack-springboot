4458fab475ba2a49f067dd96f839674839190d5b
Manish99311 <52909385+Manish99311@users.noreply.github.com>
2021-03-27 23:24:05 +0530

    encoded the password with bcrypt

--- backend/backend/src/main/java/com/manishtech/backend/service/AuthService.java
+++ backend/backend/src/main/java/com/manishtech/backend/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.manishtech.backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.manishtech.backend.Dao.UserRepository;
@@ -14,13 +15,21 @@ public class AuthService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	public void singup(RegisterRequest registerRequest) {
 		
 		User user = new User();
 		user.setName(registerRequest.getName());
 		user.setEmailId(registerRequest.getEmailId());
-		user.setPassword(registerRequest.getPassword());
+		user.setPassword(encodePassword(registerRequest.getPassword()));
 		userRepository.save(user);
+	}
+
+	private String encodePassword(String password) {
+		// TODO Auto-generated method stub
+		return passwordEncoder.encode(password); 
 	}
 
 }
